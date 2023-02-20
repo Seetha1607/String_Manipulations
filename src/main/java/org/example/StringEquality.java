4fe5acbd0435420883d6278e90118499b6376bd4
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2023-02-20 19:28:52 +0530

    updated the equals() and operator checking for string equality

--- src/main/java/org/example/StringEquality.java
+++ src/main/java/org/example/StringEquality.java
@@ -12,10 +12,12 @@ public class StringEquality {
     public static void main(String[] args) {
 
         /*String objects created using new keyword will have different memory
-        references. So it will not be equal */
+        references. So it will not be equal
+        If you use '==' operator, it will not be same.
+        If you use equals() method, it will be same whether the memory reference is same or not */
         String name = new String("Seetha");
         String name1 = new String("Seetha");
-        if (name == name1) {
+        if (name.equals(name1)) {
             System.out.println("Same");
         } else {
             System.out.println("Not Same");
@@ -25,7 +27,7 @@ public class StringEquality {
         references. So it will equal */
         String name2 = "Seetha";
         String name3 = "Seetha";
-        if (name2 == name3) {
+        if (name2.equals(name3)) {
             System.out.println("Same");
         } else {
             System.out.println("Not Same");
