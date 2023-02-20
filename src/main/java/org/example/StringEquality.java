e095a5a47e92977c19a881b4fe72433a5282924d
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2023-02-20 19:23:13 +0530

    checked the equality of Strings using String object and String literals

--- src/main/java/org/example/StringEquality.java
+++ src/main/java/org/example/StringEquality.java
@@ -8,4 +8,28 @@
 package org.example;
 
 public class StringEquality {
+
+    public static void main(String[] args) {
+
+        /*String objects created using new keyword will have different memory
+        references. So it will not be equal */
+        String name = new String("Seetha");
+        String name1 = new String("Seetha");
+        if (name == name1) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not Same");
+        }
+
+        /*String objects created using String literals will have same memory
+        references. So it will equal */
+        String name2 = "Seetha";
+        String name3 = "Seetha";
+        if (name2 == name3) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not Same");
+        }
+    }
 }
+
